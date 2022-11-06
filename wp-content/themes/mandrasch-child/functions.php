883bd51a455fddf2d74000f66236124fe844789c
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-11-06 09:08:15 +0100

    add child theme basic files

---
+++ wp-content/themes/mandrasch-child/functions.php
@@ -0,0 +1,7 @@
+<?php
+// https://www.webtimiser.de/wordpress-child-theme-erstellen/
+function child_theme_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-theme-css', get_stylesheet_directory_uri() .'/style.css' , array('parent-style'));
+}
+add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
