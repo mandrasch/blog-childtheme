443edc042ca0361a72ec292fda80412815fa21ab
Matthias Andrasch <777278+mandrasch@users.noreply.github.com>
2022-11-06 10:21:06 +0100

    add archive header title via hook + more improvements

---
+++ README.md
@@ -0,0 +1,5 @@
+Child theme for my personal blog.
+
+Local development via DDEV and ddev pull, see https://ddev-pull-wp.mandrasch.eu/.
+
+Deployment via https://github.com/SayHelloGmbH/git-installer.
--- wp-content/themes/mandrasch-child/functions.php
+++ wp-content/themes/mandrasch-child/functions.php
@@ -5,3 +5,9 @@ function child_theme_styles() {
     wp_enqueue_style( 'child-theme-css', get_stylesheet_directory_uri() .'/style.css' , array('parent-style'));
 }
 add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
+
+// Archive title in Eksell couldn't have HTML in it, but I needed a link: 
+function mandrasch_add_header_title(){
+    echo '<div class="archive-title i-a a-fade-up" style="font-size:2rem;">Hi! This is my personal blog for general notes and thoughts. My developer portfolio is located at <a href="https://mandrasch.dev">mandrasch.dev</a> and I share development notes on <a href="https://dev.to/mandrasch/">dev.to/mandrasch</a>.</div>';
+}
+add_action('eksell_archive_header_start', 'mandrasch_add_header_title');
